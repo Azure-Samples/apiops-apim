9b5adc1ac6fff528c1b7fd4783503b8869c2d45a
Kshitij Sharma <kshitij.sharma@sitecore.com>
2024-07-25 11:16:32 +0530

    Update api_builder.py (#12)

--- api_management/deployment/builders/api_builder.py
+++ api_management/deployment/builders/api_builder.py
@@ -1,20 +1,13 @@
 import os
-from azure.mgmt.apimanagement import ApiManagementClient
 from deployment.utils import load_json_file, load_text_file
 from deployment.logger import get_logger
+from deployment.builders.builder_base import BuilderBase
 
 logger = get_logger()
 
 
-class ApiBuilder:
-    def __init__(
-        self, client: ApiManagementClient, resource_group: str, apim_instance: str
-    ):
-        self.client = client
-        self.resource_group = resource_group
-        self.apim_instance = apim_instance
-
-    def deploy(self, environment):
+class ApiBuilder(BuilderBase):
+    def create(self, environment: str):
         api_folder_base = os.path.join(
             "environments", environment, self.apim_instance, "apis"
         )
@@ -40,4 +33,14 @@ class ApiBuilder:
                 logger.info(f"Successfully deployed API {api_name}")
         except Exception as e:
             logger.error(f"Error deploying API {api_name}: {e}")
+            raise
+
+    def delete(self, resource_name: str):
+        try:
+            self.client.api.delete(
+                self.resource_group, self.apim_instance, resource_name, if_match="*"
+            )
+            logger.info(f"Deleted API {resource_name}")
+        except Exception as e:
+            logger.error(f"Error deleting API {resource_name}: {e}")
             raise

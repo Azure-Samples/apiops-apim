ebc0f076ac84e973f750d5f45feebe6352df16fb
Kshitij Sharma <kshi@sitecore.net>
2024-08-07 15:06:07 +0530

    Initial testing scenario #1

--- api_management/tests/integeration/conftest.py
+++ api_management/tests/integeration/conftest.py
@@ -69,6 +69,7 @@ def setup_and_teardown_product(builder_factory):
         response = list(product_builder.client.product_api.list_by_product(
             builder_factory.resource_group, builder_factory.apim_instance, product_id
         ))
+        assert response is not None
         assert response[0].name == api_id
     yield
 
